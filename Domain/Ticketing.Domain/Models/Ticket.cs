9de8d2621f1d4ce96b23d5b34a591ad8facbae9a
João Luizi <joao.luizi@gmail.com>
2026-06-29 13:09:17 +0100

    feat(domain): add Ticketing.Domain project,  add Ticket entity

---
+++ Domain/Ticketing.Domain/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Domain.Models
+{
+    public class Ticket
+    {
+        int Id { get; set; }
+        string Title { get; set; } = string.Empty;  
+        string Description { get; set; } = string.Empty;
+        string UserName { get; set; } = string.Empty;
+        string UserEmail { get; set; } = string.Empty;
+        DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        DateTime? ClosedAt { get; set; }
+
+    }
+}
