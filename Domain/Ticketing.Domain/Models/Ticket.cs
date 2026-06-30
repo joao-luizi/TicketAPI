85dc12cf7d91ce2a3e98324b4a2fcf37038835b3
João Luizi <joao.luizi@gmail.com>
2026-06-30 10:55:42 +0100

    feat (domain) updated Models Ticket and add user entity

--- Domain/Ticketing.Domain/Models/Ticket.cs
+++ Domain/Ticketing.Domain/Models/Ticket.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ticketing.Domain.Enums;
 
 namespace Ticketing.Domain.Models
 {
     public class Ticket
     {
-        int Id { get; set; }
-        string Title { get; set; } = string.Empty;  
-        string Description { get; set; } = string.Empty;
-        string UserName { get; set; } = string.Empty;
-        string UserEmail { get; set; } = string.Empty;
-        DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        DateTime? ClosedAt { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public TicketStatus TicketStatus { get; set; } = TicketStatus.Created;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ClosedAt { get; set; }
 
     }
 }
---
+++ Domain/Ticketing.Domain/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+        public bool EmailConfirmed { get; set; }
+
+        public int FailedLoginAttempts { get; set; }
+        public DateTime? LockoutEnd { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastLoginAt { get; set; }
+    }
+}
