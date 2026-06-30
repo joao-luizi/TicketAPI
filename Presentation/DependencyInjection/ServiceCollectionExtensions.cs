2d8fccd3799388609376fe819e7b0a83c65b4c79
João Luizi <joao.luizi@gmail.com>
2026-06-30 12:26:57 +0100

    feat  (presentation) add ServiceCollectionExtension

---
+++ Presentation/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Ticketing.Api.Validators;
+namespace Ticketing.Api.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApi(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<CreateTicketRequestValidator>();
+            services.AddValidatorsFromAssemblyContaining<Program>();
+            return services;
+        }
+    }
+}
