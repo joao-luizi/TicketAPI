1a6886873a897f91de62d9890d6604c2933f102c
João Luizi <joao.luizi@gmail.com>
2026-06-30 10:54:44 +0100

    feat (domain) add TicketStatus enum

---
+++ Domain/Ticketing.Domain/Enums/TicketStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Domain.Enums
+{
+    public enum TicketStatus
+    {
+        Created = 0,
+        Closed = 1,
+        Canceled = 2,
+    }
+}
