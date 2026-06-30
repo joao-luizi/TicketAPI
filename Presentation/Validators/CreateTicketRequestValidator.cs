008541d927144b807818126ff1776eb84c9d577d
João Luizi <joao.luizi@gmail.com>
2026-06-30 12:16:42 +0100

    feat (presentation) add CreateTicketRequestValidator

---
+++ Presentation/Validators/CreateTicketRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Ticketing.Api.Contracts.Request;
+using FluentValidation;
+
+namespace Ticketing.Api.Validators
+{
+    public sealed class CreateTicketRequestValidator : AbstractValidator<CreateTicketRequest>
+    {
+       public CreateTicketRequestValidator() 
+        { 
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(100).WithMessage("Title cannot exceed 100 characters.");
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+            RuleFor(x => x.UserEmail)
+                .NotEmpty().WithMessage("User email is required.")
+                .EmailAddress().WithMessage("Invalid email address.");
+        }
+    }
+}
