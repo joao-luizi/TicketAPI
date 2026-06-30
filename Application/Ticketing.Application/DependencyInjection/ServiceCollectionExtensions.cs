8812c65a5869d99b861e288c221dc0ec8098fcad
João Luizi <joao.luizi@gmail.com>
2026-06-30 14:20:52 +0100

    feat (application) add ServiceCollectionExtension

---
+++ Application/Ticketing.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ticketing.Application.UseCases.CreateTicket;
+
+
+namespace Ticketing.Application.DependencyInjection
+{
+    public static  class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<ICreateTicketUseCase, CreateTicketUseCase>();
+            return services;
+        }
+    }
+}
