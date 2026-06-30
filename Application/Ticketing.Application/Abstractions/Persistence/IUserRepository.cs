49658d9125d1f2f5e64a03ded1d46ae46dc2bdeb
João Luizi <joao.luizi@gmail.com>
2026-06-30 11:02:17 +0100

    feat (application) add IUserRepository

---
+++ Application/Ticketing.Application/Abstractions/Persistence/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticketing.Domain.Models;
+
+namespace Ticketing.Application.Abstractions.Persistence
+{
+    public  interface IUserRepository
+    {
+        Task<User?> GetUserById(int id, CancellationToken token);
+        Task<User?> GetUserByEmail(string email, CancellationToken token);
+
+        Task<User?> GetUserByUserName(string username, CancellationToken token);
+
+    }
+}
