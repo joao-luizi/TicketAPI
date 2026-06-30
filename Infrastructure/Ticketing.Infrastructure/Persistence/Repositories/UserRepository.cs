c448595247aecbc733d859a97571cbdb5547bba6
João Luizi <joao.luizi@gmail.com>
2026-06-30 11:02:49 +0100

    feat (Infrastructure) add UserRepository

---
+++ Infrastructure/Ticketing.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticketing.Application.Abstractions.Persistence;
+using Ticketing.Domain.Models;
+
+namespace Ticketing.Infrastructure.Persistence.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        private List<User> users = new List<User>();
+        int currentId = 0;
+
+        public async Task<User?> GetUserByEmail(string email, CancellationToken token)
+        {
+            return users.FirstOrDefault(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public async Task<User?> GetUserById(int id, CancellationToken token)
+        {
+            return users.FirstOrDefault(u => u.Id == id);
+
+        }
+
+        public async Task<User?> GetUserByUserName(string username, CancellationToken token)
+        {
+            return users.FirstOrDefault(u => u.UserName.Equals(username, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
