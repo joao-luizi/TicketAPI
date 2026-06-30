4d7b76e87a3a091d419eedfb78d5d1b60f8b5198
João Luizi <joao.luizi@gmail.com>
2026-06-30 10:57:07 +0100

    feat (Application) add ITicketRepository

---
+++ Application/Ticketing.Application/Abstractions/Persistence/ITicketRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticketing.Domain.Models;
+
+namespace Ticketing.Application.Abstractions.Persistence
+{
+    public interface ITicketRepository
+    {
+         Task<Ticket> CreateTicketAsync(Ticket ticket, CancellationToken cancellationToken);
+
+
+        Task<bool> HasDuplicateTicketAsync(int userId, string title, CancellationToken cancellationToken);
+       
+    }
+}
