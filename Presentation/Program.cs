e3c98636f159561113c1552a34bca120483c8d59
João Luizi <joao.luizi@gmail.com>
2026-06-30 12:28:19 +0100

    feat (presentation) register builder.Services.AddApi()

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,9 +1,11 @@
+using Ticketing.Api.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
