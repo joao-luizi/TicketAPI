4b0f3f9621f9b1fdbb2ef45101de36e013330c4f
João Luizi <joao.luizi@gmail.com>
2026-06-30 11:59:22 +0100

    feat (Presentation) add CreateTicketRequest, add CreateTicketResponse

---
+++ Presentation/Contracts/Request/CreateTicketRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticketing.Api.Contracts.Request
+{
+    public class CreateTicketRequest
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+    }
+}
---
+++ Presentation/Contracts/Response/CreateTicketResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticketing.Api.Contracts.Response
+{
+    public class CreateTicketResponse
+    {
+        public bool Success { get; set; }
+        public int TicketId { get; set; }
+        public string? Detail { get; set; }
+    }
+}
