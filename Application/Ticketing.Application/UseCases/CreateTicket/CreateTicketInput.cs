6bee231f27dc9af8a9901885dccf7a5bfb2def04
João Luizi <joao.luizi@gmail.com>
2026-06-30 10:54:07 +0100

    feat(application): add Ticket DTOs

---
+++ Application/Ticketing.Application/UseCases/CreateTicket/CreateTicketInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Application.UseCases.CreateTicket
+{
+    public class CreateTicketInput
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+    }
+}
---
+++ Application/Ticketing.Application/UseCases/CreateTicket/CreateTicketOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Application.UseCases.CreateTicket
+{
+    public class CreateTicketOutput
+    {
+        public bool Success { get; set; }
+        public int TicketId { get; set; }
+        public string? Detail { get; set; }
+    }
+}
