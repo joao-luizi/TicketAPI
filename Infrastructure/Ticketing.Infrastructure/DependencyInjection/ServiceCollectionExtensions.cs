128a8c58274be63b6791bc0e8b2a087ab9f180aa
João Luizi <joao.luizi@gmail.com>
2026-06-30 14:21:20 +0100

    feat (infrastructure) add ServiceCollectionExtension

---
+++ Infrastructure/Ticketing.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ticketing.Application.Abstractions.Persistence;
+using Ticketing.Infrastructure.Persistence.Repositories;
+
+namespace Ticketing.Infrastructure.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfra(this IServiceCollection services)
+        {
+            services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            return services;
+        }
+    }
+}
