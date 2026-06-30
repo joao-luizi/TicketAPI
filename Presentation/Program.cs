aa02ad1a0953b1b8200a3f5bba5277869b3e1526
João Luizi <joao.luizi@gmail.com>
2026-06-30 12:37:16 +0100

    feat (presentation) add Swagger

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -4,17 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+//builder.Services.AddOpenApi();
 builder.Services.AddApi();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapControllers();
 
 
 app.Run();
