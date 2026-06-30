b93ed097511d4552e30d60e246e82f73e4bbf745
João Luizi <joao.luizi@gmail.com>
2026-06-30 11:59:44 +0100

    feat (presentation) add HealthController

---
+++ Presentation/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticketing.Api.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    [Tags("Health")]
+    public sealed class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Status = "Healthy",
+                UtcNow = DateTimeOffset.UtcNow
+            });
+        }
+    }
+}
