61955cfaed11d70365d31d5bfe57e91ce6458192
João Luizi <joao.luizi@gmail.com>
2026-06-30 12:00:10 +0100

    feat (presentation) add TicketController

---
+++ Presentation/Controllers/TicketController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.Api.Contracts.Request;
+using Ticketing.Api.Contracts.Response;
+using Ticketing.Application.UseCases.CreateTicket;
+
+namespace Ticketing.Api.Controllers
+{
+    [ApiController]
+    [Route("tickets")]
+    [Tags("Tickets")]
+    public class TicketController(ICreateTicketUseCase createTicketUseCase) : ControllerBase
+    {
+        [HttpPost("create")]
+        [ProducesResponseType(typeof(CreateTicketResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CreateTicketResponse>> Process(
+        [FromBody] CreateTicketRequest request,
+        CancellationToken cancellationToken)
+        {
+            try
+            {
+                var input = new CreateTicketInput()
+                {
+                    Title = request.Title,
+                    Description = request.Description,
+                    UserEmail = request.UserEmail
+                };
+
+                var output = await createTicketUseCase.CreateTicketAsync(input, cancellationToken);
+                
+                return new CreateTicketResponse
+                {
+                    Success = output.Success,
+                    TicketId = output.TicketId,
+                    Detail = output.Detail
+                };
+            }
+            catch(Exception ex)
+            {
+                return Problem(title: "Failed to process Create Ticket request", detail: ex.Message);
+            }
+        }
+    }
+}
