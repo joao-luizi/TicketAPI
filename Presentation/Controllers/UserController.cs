8ca5925b6091c61796915c3a8cd5c79961c8c2a0
João Luizi <joao.luizi@gmail.com>
2026-07-01 16:42:08 +0100

    [ADD] UserController

---
+++ Presentation/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.Api.Contracts.Request;
+using Ticketing.Api.Contracts.Response;
+using Ticketing.Application.UseCases.CreateTicket;
+using Ticketing.Application.UseCases.CreateUser;
+
+namespace Ticketing.Api.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    [Tags("Users")]
+    public class UserController(ICreateUserUseCase createUserUseCase) : ControllerBase
+    {
+        [HttpPost("create")]
+        [ProducesResponseType(typeof(CreateUserResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CreateUserResponse>> Process(
+        [FromBody] CreateUserRequest request,
+        CancellationToken cancellationToken)
+        {
+            try
+            {
+                var input = new CreateUserInput()
+                {
+                   UserName = request.username,
+                   Email = request.email,
+                   Password = request.password
+                };
+
+                var output = await createUserUseCase.CreateUserAsync(input, cancellationToken);
+
+                return new CreateUserResponse
+                {
+                    Success = output.Success,
+                    UserId = output.UserId,
+                    Detail = output.Detail
+                };
+            }
+            catch (Exception ex)
+            {
+                return Problem(title: "Failed to process Create User request", detail: ex.Message);
+            }
+        }
+    }
+}
