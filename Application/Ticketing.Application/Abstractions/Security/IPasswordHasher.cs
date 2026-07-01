b4ad4bdf7a261403c26647713348d8b4b44ad6a5
João Luizi <joao.luizi@gmail.com>
2026-07-01 16:39:20 +0100

    [ADD] IPasswordHasher [ADD] BcryptPasswordHasher

---
+++ Application/Ticketing.Application/Abstractions/Security/IPasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Application.Abstractions.Security
+{
+
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string password, string passwordHash);
+    }
+
+}
