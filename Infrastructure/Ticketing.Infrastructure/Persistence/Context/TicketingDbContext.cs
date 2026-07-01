216ada002b879dfc098650f064aa8f9a85893d48
João Luizi <joao.luizi@gmail.com>
2026-07-01 09:47:08 +0100

    feat (Infrastructure) add TicketingDbContext

---
+++ Infrastructure/Ticketing.Infrastructure/Persistence/Context/TicketingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Domain.Models;
+
+
+namespace Ticketing.Infrastructure.Persistence.Context
+{
+    public sealed class TicketingDbContext(DbContextOptions<TicketingDbContext> options) : DbContext(options)
+    {
+        public DbSet<Ticket> Tickets => Set<Ticket>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TicketingDbContext).Assembly);
+        }
+    }
+
+}
