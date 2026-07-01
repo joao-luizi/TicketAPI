4679897ec1deab4e7c642c82287515d4ca06a41a
João Luizi <joao.luizi@gmail.com>
2026-07-01 14:35:29 +0100

    feat (domain) Add user Entity

--- Domain/Ticketing.Domain/Models/User.cs
+++ Domain/Ticketing.Domain/Models/User.cs
@@ -10,16 +10,11 @@ namespace Ticketing.Domain.Models
 
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-
         public string PasswordHash { get; set; } = string.Empty;
 
         public bool IsActive { get; set; } = true;
-        public bool EmailConfirmed { get; set; }
-
-        public int FailedLoginAttempts { get; set; }
-        public DateTime? LockoutEnd { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? LastLoginAt { get; set; }
+ 
     }
 }
