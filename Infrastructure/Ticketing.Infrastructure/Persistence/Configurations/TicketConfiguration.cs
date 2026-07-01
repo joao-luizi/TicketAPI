10820e92b05aaf92a055d41258f9aa1a66108b69
João Luizi <joao.luizi@gmail.com>
2026-07-01 09:47:36 +0100

    feat (infrastructure) add TicketConfiguration

---
+++ Infrastructure/Ticketing.Infrastructure/Persistence/Configurations/TicketConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Domain.Models;
+
+namespace Ticketing.Infrastructure.Persistence.Configurations
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Ticket> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Title).IsRequired().HasMaxLength(100);
+            builder.Property(t => t.Description).IsRequired().HasMaxLength(500);
+            builder.Property(t => t.TicketStatus).IsRequired();
+            builder.Property(t => t.CreatedAt).IsRequired();
+
+        }
+    }
+}
+
