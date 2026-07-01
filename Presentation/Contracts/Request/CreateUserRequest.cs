64878db3a5d3b4014d997e0ee853ae777be653b1
João Luizi <joao.luizi@gmail.com>
2026-07-01 15:54:00 +0100

    [ADD] CreateUSerRequest, CreateUserResponse

---
+++ Presentation/Contracts/Request/CreateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Ticketing.Api.Contracts.Request
+{
+    public class CreateUserRequest
+    {
+        public string username { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+
+        public string fullName { get; set; } = string.Empty;
+
+    }
+}
---
+++ Presentation/Contracts/Response/CreateUserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticketing.Api.Contracts.Response
+{
+    public class CreateUserResponse
+    {
+        public bool Success { get; set; }
+        public int UserId { get; set; }
+        public string? Detail { get; set; }
+    }
+}
