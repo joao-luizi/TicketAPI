638d91c243228062651495aa70d706af948b43a1
João Luizi <joao.luizi@gmail.com>
2026-07-01 14:36:21 +0100

    feat (infrastructure) add user configuration and add user migration

---
+++ Infrastructure/Ticketing.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ticketing.Domain.Models;
+
+namespace Ticketing.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.UserName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.HasIndex(u => u.UserName)
+                .IsUnique();
+
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
--- Infrastructure/Ticketing.Infrastructure/Persistence/Context/TicketingDbContext.cs
+++ Infrastructure/Ticketing.Infrastructure/Persistence/Context/TicketingDbContext.cs
@@ -7,6 +7,7 @@ namespace Ticketing.Infrastructure.Persistence.Context
     public sealed class TicketingDbContext(DbContextOptions<TicketingDbContext> options) : DbContext(options)
     {
         public DbSet<Ticket> Tickets => Set<Ticket>();
+        public DbSet<User> Users => Set<User>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
