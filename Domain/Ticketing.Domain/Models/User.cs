d99cc68679315422604163d02a6d97b6cd6e5153
João Luizi <joao.luizi@gmail.com>
2026-07-02 12:42:10 +0100

    [ADD] UserRoles added to entiry User

--- Domain/Ticketing.Domain/Models/User.cs
+++ Domain/Ticketing.Domain/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ticketing.Domain.Enums;
 
 namespace Ticketing.Domain.Models
 {
@@ -11,6 +12,7 @@ namespace Ticketing.Domain.Models
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+        public UserRoles UserRoles { get; set; } = UserRoles.User;
 
         public bool IsActive { get; set; } = true;
 
