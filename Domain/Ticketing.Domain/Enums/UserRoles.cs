6b508753227add54cb7677970d090bee07f23040
João Luizi <joao.luizi@gmail.com>
2026-07-02 12:41:48 +0100

    [ADD] add userRoles Enum

---
+++ Domain/Ticketing.Domain/Enums/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Domain.Enums
+{
+    public enum  UserRoles
+    {
+        User = 0,
+        Admin = 1,
+    }
+}
