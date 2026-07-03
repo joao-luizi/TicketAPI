b921ee9aaea6d0c32b9304773653c65c2b76a9a2
João Luizi <joao.luizi@gmail.com>
2026-07-03 08:48:31 +0100

    [ADD] JwtOption, JwtTokenService

---
+++ Infrastructure/Ticketing.Infrastructure/Services/Security/JwtOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Infrastructure.Services.Security
+{
+
+    public sealed class JwtOptions
+    {
+        public const string SectionName = "Jwt";
+
+        public string Key { get; init; } = string.Empty;
+        public string Issuer { get; init; } = string.Empty;
+        public string Audience { get; init; } = string.Empty;
+        public int ExpiryMinutes { get; init; }
+    }
+
+}
--- Infrastructure/Ticketing.Infrastructure/Services/Security/JwtTokenService.cs
+++ Infrastructure/Ticketing.Infrastructure/Services/Security/JwtTokenService.cs
@@ -1,15 +1,40 @@
-﻿using System.Security.Claims;
+﻿
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Ticketing.Application.Abstractions.Security;
 using Ticketing.Domain.Models;
 
 namespace Ticketing.Infrastructure.Services.Security
 {
-    internal class JwtTokenService : ITokenService
+    public class JwtTokenService(IOptions<JwtOptions> option) : ITokenService
     {
         public string GenerateAccessToken(User user)
         {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.UserRoles.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
 
-            throw new NotImplementedException();
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(option.Value.Key));
+
+            var credentials = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: option.Value.Issuer,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         public string GenerateRefreshToken()
