af5a3171726e3ba8442847e8c4220272ad592414
João Luizi <joao.luizi@gmail.com>
2026-07-03 09:26:47 +0100

    [ADD] Enum signaling fail type in create ticket

---
+++ Domain/Ticketing.Domain/Enums/CreateTicketFailureType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Domain.Enums
+{
+    public enum CreateTicketFailureType
+    {
+        None,
+        UserNotFound,
+        UserInactive,
+        DuplicateTicket
+    }
+}
