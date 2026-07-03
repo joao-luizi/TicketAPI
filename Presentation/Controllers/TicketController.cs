cbb40da68d8d20853f952916ee58ba631b15f875
João Luizi <joao.luizi@gmail.com>
2026-07-03 09:28:13 +0100

    [ADD] Status409Conflict to Controler

--- Presentation/Controllers/TicketController.cs
+++ Presentation/Controllers/TicketController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Ticketing.Api.Contracts.Request;
 using Ticketing.Api.Contracts.Response;
 using Ticketing.Application.UseCases.Ticket.CreateTicket;
+using Ticketing.Domain.Enums;
 
 namespace Ticketing.Api.Controllers
 {
@@ -13,6 +14,7 @@ namespace Ticketing.Api.Controllers
     {
         [HttpPost]
         [ProducesResponseType(typeof(CreateTicketResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(CreateTicketResponse), StatusCodes.Status409Conflict)]
         [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<CreateTicketResponse>> Process(
@@ -29,22 +31,28 @@ namespace Ticketing.Api.Controllers
                 };
 
                 var output = await createTicketUseCase.Execute(input, cancellationToken);
-                
-                return Ok(new CreateTicketResponse
+                var response = new CreateTicketResponse()
                 {
                     Success = output.Success,
                     TicketId = output.TicketId,
                     Detail = output.Detail
-                });
+                };
+
+                if (!output.Success)
+                {
+                    return output.FailureType switch
+                    {
+                        CreateTicketFailureType.DuplicateTicket => Conflict(response),
+                        _ => BadRequest(response)
+                    };
+                }
+
+                return Ok(response);
             }
             catch(Exception ex)
             {
                 return Problem(title: "Failed to process Create Ticket request", detail: ex.Message);
             }
         }
-        
-
-        
-
     }
 }
