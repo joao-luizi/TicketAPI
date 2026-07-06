5d864f54c98d9cc65d3544d21464aef713f6eedf
João Luizi <joao.luizi@gmail.com>
2026-07-06 13:22:03 +0100

    [ADD] add infrastructure ServiceCollection tests

---
+++ Ticketing.Infrastructure.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ticketing.Infrastructure.DependencyInjection;
+
+namespace Ticketing.Infrastructure.Tests
+{
+    public  class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddInfrastructure_ShouldThrow_WhenConnectionStringIsMissing()
+        {
+            var services = new ServiceCollection();
+            var configuration = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string?>()).Build();
+
+            Assert.Throws<InvalidOperationException>(() => services.AddInfra(configuration));
+        }
+    }
+}
